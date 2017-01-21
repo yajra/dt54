b7ab52e867b1efc5c4be686bf137a4fcf28f8adf
Arjay Angeles <aqangeles@gmail.com>
2017-01-21 15:06:35 +0800

    Fix post factory.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,9 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
@@ -26,6 +26,5 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 $factory->define(App\Post::class, function (Faker\Generator $faker) {
     return [
         'title' => $faker->sentence(),
-        'content' => $faker->paragraph(),
     ];
 });
