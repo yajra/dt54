adaf2ead096f2f3845a57efa5a2801ee44615287
Arjay Angeles <aqangeles@gmail.com>
2017-01-21 15:23:54 +0800

    Add 404 page handler.

--- resources/views/eloquent/partials/menu.blade.php
+++ resources/views/eloquent/partials/menu.blade.php
@@ -3,3 +3,7 @@
 <li class="list-group-item"><a href="/eloquent/object">Object Response</a></li>
 <li class="list-group-item-heading"><h4>Eager Loading</h4></li>
 <li class="list-group-item"><a href="/eloquent/belongs-to">Belongs To</a></li>
+<li class="list-group-item"><a href="/eloquent/belongs-to-many">Belongs To Many</a></li>
+<li class="list-group-item"><a href="/eloquent/has-one">Has One</a></li>
+<li class="list-group-item"><a href="/eloquent/has-many">Has Many</a></li>
+<li class="list-group-item"><a href="/eloquent/morph-to-many">Morph To Many</a></li>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@push('styles')
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<style>
+    html, body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+    }
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
+@endpush
+
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <h3>Whoops! Page Not Found!</h3>
+            <div class="title m-b-md">
+                Laravel DataTables
+            </div>
+
+            <div class="links">
+                <a href="https://yajrabox.com/docs/laravel-datatables/master">Documentation</a>
+                <a href="https://laravel.com/docs">Laravel</a>
+                <a href="https://datatables.net">DataTables.net</a>
+                <a href="https://github.com/yajra/laravel-datatables">GitHub</a>
+            </div>
+        </div>
+    </div>
+@endsection
